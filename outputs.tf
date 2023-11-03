b7c06458c68076d2334bfc0e44659cab5a57180c
gowripadmajas <gowris@sph.com.sg>
2023-11-03 16:12:13 +0800

    fix output format for workspaces

--- outputs.tf
+++ outputs.tf
@@ -5,18 +5,17 @@ output "auth_mount_accessor" {
 
 output "workspaces" {
   description = "Workspace information"
-  value = { for k, v in local.workspaces : v.org => tomap( { (v.ws) = merge(
-      {
-        org       = v.org
-        project   = v.proj
-        workspace = v.ws
-        role      = vault_jwt_auth_backend_role.roles[k].role_name
-      },
-      var.enable_identity_management ? {
-        identity_name  = vault_identity_entity.workspaces[k].name
-        identity_id    = vault_identity_entity.workspaces[k].id
-        identity_alias = vault_identity_entity_alias.workspaces[k].name
-      } : {}
-    ) })
-  }
+  value = { for k, v in local.workspaces : v.org => tomap({ (v.ws) = merge(
+    {
+      org       = v.org
+      project   = v.proj
+      workspace = v.ws
+      role      = vault_jwt_auth_backend_role.roles[k].role_name
+    },
+    var.enable_identity_management ? {
+      identity_name  = vault_identity_entity.workspaces[k].name
+      identity_id    = vault_identity_entity.workspaces[k].id
+      identity_alias = vault_identity_entity_alias.workspaces[k].name
+    } : {}
+  ) })... }
 }
